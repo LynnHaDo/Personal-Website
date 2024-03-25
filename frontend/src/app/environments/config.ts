cabe3183a3c7034083231bc81bbd48f0e07810ed
LynnHaDo <do24l@mtholyoke.edu>
2024-03-25 00:27:13 -0400

    Reconfigure project list

--- frontend/src/app/environments/config.ts
+++ frontend/src/app/environments/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-    projectUrl = "postgres://default:YNz7qCnMp1sE@ep-morning-night-a4f6xhk7-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require"
-    static projectUrl = "postgres://default:YNz7qCnMp1sE@ep-morning-night-a4f6xhk7-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require"
+    projectUrl = ""
+    static projectUrl = ""
 }
--- frontend/src/app/services/project-list.service.ts
+++ frontend/src/app/services/project-list.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Config } from "../environments/config";
 import { Project } from "../models/project";
+import { sql } from "@vercel/postgres";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProjectListService {
-  projectsUrl = Config.projectUrl;
+  projects = sql`SELECT * from Projects`;
   constructor(private httpClient: HttpClient) {}
 
   getProjects(){
-    return this.httpClient.get<Project[]>(this.projectsUrl)
+    return this.projects
   }
 
 }
